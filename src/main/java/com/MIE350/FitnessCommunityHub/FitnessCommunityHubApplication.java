c8172afacb9f3c62dfacf1aff6913f2fca1c5914
LoganZ2 <loganzhuang123@gmail.com>
2025-01-24 03:21:38 -0500

    Init

---
+++ src/main/java/com/MIE350/FitnessCommunityHub/FitnessCommunityHubApplication.java
@@ -0,0 +1,13 @@
+package com.MIE350.FitnessCommunityHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCommunityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessCommunityHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MIE350/FitnessCommunityHub/FitnessCommunityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MIE350.FitnessCommunityHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCommunityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
